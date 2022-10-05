6ab51e31d248ea06881c2319d7f81dcfded6975a
yangu1455 <kunja266@hanmail.net>
2022-10-05 23:59:01 +0900

    221005 Django 10일차 실습 중

---
+++ 221005 Django 10일차/pjt_1004/practice/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from .models import Article
+from .forms import ArticleForm
+
+# Create your views here.
+def index(request):
+    article = Article.objects.order_by("-pk")
+    context = {
+        "article": article,
+    }
+    return render(request, "practice/index.html", context)
+
+
+# def new(request):
+#     article_form = ArticleForm()
+#     context = {
+#         'article_form': article_form
+#     }
+#     return render(request, 'articles/new.html', context=context)
+
+
+def create(request):
+    # title = request.POST.get("title")
+    # content = request.POST.get("content")
+    # Article.objects.create(title=title, content=content)
+    # return redirect("practice:index")
+
+    if request.method == "POST":
+        # DB에 저장하는 로직
+        article_form = ArticleForm(request.POST)
+
+        if article_form.is_valid():
+            article_form.save()
+            return redirect("practice:index")
+
+    else:
+        article_form = ArticleForm()
+
+    context = {
+        "article_form": article_form,
+    }
+    return render(request, "practice/new.html", context=context)
