8ddbfa70ca2876892a152b5d8b042008d3d667b5
yangu1455 <kunja266@hanmail.net>
2022-10-06 23:52:32 +0900

    221006 실습중

---
+++ 221006 Django 11일차/jango09_pjt/movies/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # 영화 데이터 목록 조회
+    path("", views.index, name="index"),
+    # 영화 데이터 정보 조회
+    path("<int:pk>/", views.select, name="select"),
+    # 영화 데이터 생성
+    path("create/", views.create, name="create"),
+    # 영화 데이터 수정
+    path("<int:pk>/update/", views.update, name="update"),
+    # 영화 데이터 삭제
+    path("<int:pk>/delete/", views.delete, name="delete"),
+]
---
+++ 221006 Django 11일차/jango09_pjt/movies/views.py
@@ -0,0 +1,24 @@
+from operator import index
+from turtle import update
+from venv import create
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    pass
+
+
+def select(request):
+    pass
+
+
+def create(request):
+    pass
+
+
+def update(request):
+    pass
+
+
+def delete(request):
+    pass
