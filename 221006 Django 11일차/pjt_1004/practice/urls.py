b665af83f8b940a6ab9ddffc3dbac7dbeb0799f8
yangu1455 <kunja266@hanmail.net>
2022-10-08 22:07:08 +0900

    _

--- 221006 Django 11일차/pjt_1004/practice/urls.py
+++ 221006 Django 11일차/pjt_1004/practice/urls.py
@@ -7,6 +7,8 @@ app_name = "practice"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("new/", views.new, name="new"),
+    # path("new/", views.new, name="new"),
     path("create/", views.create, name="create"),
+    path("<int:pk>/", views.detail, name="detail"),
+    path("<int:pk>/update", views.update, name="update"),
 ]
--- 221006 Django 11일차/pjt_1004/practice/views.py
+++ 221006 Django 11일차/pjt_1004/practice/views.py
@@ -11,27 +11,45 @@ def index(request):
     return render(request, "practice/index.html", context)
 
 
-def new(request):
-    # article_form = ArticleForm()
-    # context = {"article_form": article_form}
-    return render(request, "practice/new.html")
+# def new(request):
+#     article_form = ArticleForm()
+#     context = {
+#         "article_form": article_form,
+#     }
+#     return render(request, "practice/new.html", context=context)
 
 
 def create(request):
-    title = request.POST.get("title")
-    content = request.POST.get("content")
-    Article.objects.create(title=title, content=content)
-    return redirect("practice:index")
+    # title = request.POST.get("title")
+    # content = request.POST.get("content")
+    # Article.objects.create(title=title, content=content)
+    # return redirect("practice:index")
 
-    # if request.method == 'POST':
-    #     # DB에 저장하는 로직
-    #     article_form = ArticleForm(request.POST)
-    #     if article_form.is_valid():
-    #         article_form.save()
-    #         return redirect('practice:index')
-    # else:
-    #     article_form = ArticleForm()
-    # context = {
-    #     'article_form': article_form
-    # }
-    # return render(request, 'practice/new.html', context=context)
+    if request.method == "POST":
+        # DB에 저장하는 로직
+        article_form = ArticleForm(request.POST)
+        # 유효성 검사
+        if article_form.is_valid():
+            # 유효하면 세이브
+            article_form.save()
+            return redirect("practice:index")
+    else:
+        article_form = ArticleForm()
+
+    context = {
+        "article_form": article_form,
+    }
+    return render(request, "practice/new.html", context=context)
+
+
+def detail(request, pk):
+    # 특정 글을 가져온다.
+    article = Article.objects.get(pk=pk)
+    context = {
+        "article": article,
+    }
+    return render(request, "practice/detail.html", context)
+
+
+def update(request, pk):
+    return render(request, "practice.update.html")
