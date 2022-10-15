7b33188d7b4354079646a82763fb97ed152aad80
yangu1455 <kunja266@hanmail.net>
2022-10-15 19:45:11 +0900

    221006 실습 끝!!

--- 221006 Django 11일차/jango09_pjt/movies/views.py
+++ 221006 Django 11일차/jango09_pjt/movies/views.py
@@ -39,9 +39,29 @@ def create(request):
     return render(request, "movies/new.html", context=context)
 
 
-def update(request):
-    pass
+def update(request, pk):
+    movie = Movie.objects.get(pk=pk)
+    if request.method == "POST":
+        # POST : input 값 가져와서 검증하고, DB에 저장
+        movie_form = MovieForm(request.POST, instance=movie)
+        # 유효성 검사
+        if movie_form.is_valid():
+            # 유효하면 세이브
+            movie_form.save()
+            # 유효성 검사 통과하면 상세보기 페이지로
+            return redirect("movies:detail", movie.pk)
+
+    # 유효성검사 통과하지 않으면 => context부터해서 오류메시지 담긴 article_form을 랜더링
+    else:
+        # GET : Form을 제공
+        movie_form = MovieForm(instance=movie)
+
+    context = {
+        "movie_form": movie_form,
+    }
+    return render(request, "movies/update.html", context)
 
 
-def delete(request):
-    pass
+def delete(request, pk):
+    Movie.objects.get(pk=pk).delete()
+    return redirect('movies:index')
