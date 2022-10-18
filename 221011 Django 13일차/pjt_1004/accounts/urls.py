d4db892d7ce4eb033dabbbcbd9bad868a9ff85b8
yangu1455 <kunja266@hanmail.net>
2022-10-19 01:56:10 +0900

    221011 오전 복습

--- 221011 Django 13일차/pjt_1004/accounts/urls.py
+++ 221011 Django 13일차/pjt_1004/accounts/urls.py
@@ -9,5 +9,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.signup, name='signup')
+    path('signup/', views.signup, name='signup'),
+    path('<int:pk>/', views.detail, name='detail'),
 ]
--- 221011 Django 13일차/pjt_1004/accounts/views.py
+++ 221011 Django 13일차/pjt_1004/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm
+# from .models import User
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def signup(request):
@@ -16,3 +18,11 @@ def signup(request):
         'form' : form
     }
     return render(request, 'accounts/signup.html', context)
+
+def detail(request, pk):
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        'user' : user,
+    }
+
+    return render(request, 'accounts/detail.html', context)
