3a0f9f52e2fb957b3c8da5fd853ff2a048508c21
yangu1455 <kunja266@hanmail.net>
2022-10-21 16:35:36 +0900

    221011 실습 완료

--- 221011 Django 13일차/userpjt/accounts/forms.py
+++ 221011 Django 13일차/userpjt/accounts/forms.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
-from .models import User
+# from .models import User
+from django.contrib.auth import get_user_model
 
 class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('username', 'email', 'password1', 'password2')
--- 221011 Django 13일차/userpjt/accounts/views.py
+++ 221011 Django 13일차/userpjt/accounts/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from multiprocessing import context
+from django.shortcuts import render, redirect
+# from django.contrib.auth.forms import UserCreationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 def signup(request):
-    form = UserCreationForm()
+    # 유효성 검사
+    if request.method == "POST":
+        # DB에 저장하는 로직
+        form = CustomUserCreationForm(request.POST)
+        # 유효성 검사
+        if form.is_valid():
+            # 유효하면 세이브
+            form.save()
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+
     context = {
-        'form': form
+        "form": form,
     }
     return render(request, 'accounts/signup.html', context)
 
+
 def index(request):
-    return render(request, 'accounts/index.html')
+    users = get_user_model().objects.order_by('pk')
+    context = {
+        'users' : users,
+    }
+    return render(request, 'accounts/index.html', context)
 
 def detail(request, pk):
-    pass
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        'user' : user
+    }
+    return render(request, 'accounts/detail.html', context)
