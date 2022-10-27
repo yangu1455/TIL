674a5cbea30c66345fe11d79fe49d78a6f063237
yangu1455 <kunja266@hanmail.net>
2022-10-28 00:32:05 +0900

    _

--- 221012 Django 14일차/login_pjt/accounts/views.py
+++ 221012 Django 14일차/login_pjt/accounts/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
-from accounts.forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def index(request):
@@ -14,7 +16,7 @@ def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         # 유효성 검사
-        if form.is_vaild():
+        if form.is_valid():
             form.save()
             # 앱의 index - 회원목록
             # return redirect('accounts:index')
@@ -29,10 +31,25 @@ def signup(request):
     return render(request, 'accounts/signup.html', context)
 
 def detail(request, pk):
-    return render(request, 'accounts/detail.html')
+    user = get_user_model().objects.get(pk=pk)
+    context = {
+        'user' : user
+    }
+    return render(request, 'accounts/detail.html', context)
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('accounts:index')
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'form' : form,
+    }
+    return render(request, 'accounts/login.html', context)
 
 def logout(request):
     pass
