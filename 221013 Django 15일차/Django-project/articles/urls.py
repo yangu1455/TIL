c6940566cfe910cb04614636260e971e879ed66d
yangu1455 <kunja266@hanmail.net>
2022-10-31 22:44:57 +0900

    수업시간했던 것들 종합 프로젝트 파일 - article 모델 마이그레이션 + 함수 깔기

--- 221013 Django 15일차/Django-project/articles/urls.py
+++ 221013 Django 15일차/Django-project/articles/urls.py
@@ -4,6 +4,18 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
-    # 회원 목록 조회
+    # 글 목록 조회
     path('', views.index, name='index'),
+    # 새 글 쓰기 창 띄우기
+    path('new/', views.new, name='new'),
+    # 새로 쓴 글 저장
+    path('create/', views.create, name='create'),
+    # 글 세부
+    path('detail/<int:pk>', views.detail, name='detail'),
+    # 글 수정하기 창
+    path('edit/<int:pk>', views.edit, name='edit'),
+    # 글 수정 기능
+    path('update/<int:pk>', views.update, name='update'),
+    # 글 삭제 기능
+    path('delete/<int:pk>', views.delete, name='delete'),
 ]
--- 221013 Django 15일차/Django-project/articles/views.py
+++ 221013 Django 15일차/Django-project/articles/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
+    return render(request, 'articles/index.html')
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    pass
+
+def detail(request, pk):
+    return render(request, 'articles/detail.html')
+
+def edit(request, pk):
+    return render(request, 'articles/edit.html')
+
+def update(request, pk):
+    pass
+
+def delete(request, pk):
     pass
