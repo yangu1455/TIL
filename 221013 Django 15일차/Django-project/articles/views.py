2d81c8e71be4cf8cfb6862359618189ccd52684a
yangu1455 <kunja266@hanmail.net>
2022-11-01 15:39:49 +0900

    수업시간했던 것들 종합 프로젝트 파일 - article new + create 만드는 중 오류

--- 221013 Django 15일차/Django-project/articles/views.py
+++ 221013 Django 15일차/Django-project/articles/views.py
@@ -1,17 +1,37 @@
 from django.shortcuts import render, redirect
+from .models import Review
 
 # Create your views here.
 def index(request):
-    return render(request, 'articles/index.html')
+    reviews = Review.objects.all()
+    context = {
+        'reviews' : reviews,
+    }
+    return render(request, 'articles/index.html', context)
+
+def detail(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        'review' : review,
+    }
+    return render(request, 'articles/detail.html', context)
 
 def new(request):
     return render(request, 'articles/new.html')
 
 def create(request):
-    pass
+    title = request.GET.get('title')
+    content = request.GET.get('content')
 
-def detail(request, pk):
-    return render(request, 'articles/detail.html')
+    Review.objects.create(title=title, content=content)
+
+    # context = {
+    #     'title' : title,
+    #     'content' : content,
+    # }
+
+    return redirect('article:index')
+    # return render(request, 'article:index', context)
 
 def edit(request, pk):
     return render(request, 'articles/edit.html')
@@ -20,4 +40,5 @@ def update(request, pk):
     pass
 
 def delete(request, pk):
-    pass
+    Review.objects.get(pk=pk).delete()
+    return redirect('articles:index')
